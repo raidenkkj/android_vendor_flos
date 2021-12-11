a6f7da9ccfeea2d237884b2863f2f8e4f4ec50eb
Sagarrokade006 <Sagarrokade006@gmail.com>
2021-12-11 01:16:41 +0000

    Drop Lineage Charger

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -35,15 +35,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     TrebuchetQuickStep
 endif
 
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation
-endif
-
 # Customizations
 PRODUCT_PACKAGES += \
     LineageNavigationBarNoHint \
