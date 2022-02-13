843cacbb36202ecf8e43d26316cda409b6d1df77
sairam1411 <majgaonkars3@gmail.com>
2022-02-13 21:02:42 +0530

    Revert "vendor: Add overlays for gapps build"
    This reverts commit 5ef0bf7d9b897d85ed457f48330415b3cbe3885d.

--- config/common.mk
+++ config/common.mk
@@ -55,53 +55,15 @@ endif
 # Gapps
 ifeq ($(WITH_GMS),true)
 $(warning Building with gapps)
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
 
-$(call inherit-product, vendor/google/common/common-vendor.mk)
-
-# Common Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    vendor/lineage/overlay-gapps/common
-
-# Exclude RRO Enforcement
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS +=  \
-    vendor/lineage/overlay-gapps/common
-
-# Dex preopt
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    NexusLauncherRelease
-
-# Add acsa property for CarrierServices
+# Gboard configuration
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.acsa=true
-
-# SetupWizard configuration
-PRODUCT_PRODUCT_PROPERTIES += \
-    setupwizard.feature.baseline_setupwizard_enabled=true \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.setupwizard.rotation_locked=true \
-    setupwizard.enable_assist_gesture_training=true \
-    setupwizard.theme=glif_v3_light \
-    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
-    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
-    setupwizard.feature.show_pixel_tos=false \
-    setupwizard.feature.show_support_link_in_deferred_setup=false \
-    setupwizard.feature.device_default_dark_mode=true
-
-# OPA configuration
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.opa.eligible_device=true
-
-# Google Play services configuration
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.com.google.clientidbase=android-google \
-    ro.error.receiver.system.apps=com.google.android.gms \
-    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent
+    ro.com.google.ime.bs_theme=true \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
 
 $(call inherit-product, vendor/lineage/config/rro_overlays.mk)
-
-ifeq ($(WITH_CORE_GMS),true)
-$(call inherit-product, vendor/google/common/common-vendor-core.mk)
-endif
 
 else
 include vendor/lawnchair/lawnchair.mk
