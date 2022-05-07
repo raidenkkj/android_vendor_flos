7b8be560fcb8e38dd52d22730ce50accfc2aae44
sairam1411 <majgaonkars3@gmail.com>
2022-05-07 23:16:18 +0530

    fork: Update build fingerprint to May 2022
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -24,7 +24,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
-BUILD_FINGERPRINT := google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys
+BUILD_FINGERPRINT := google/raven/raven:12/SP2A.220505.002/8353555:user/release-keys
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
