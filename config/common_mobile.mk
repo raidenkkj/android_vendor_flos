080a8dd362831e47cd280165bfa1c56a65b7237e
sairam1411 <majgaonkars3@gmail.com>
2022-05-08 10:38:06 +0530

    fork: Guard default tones for vanilla build
    Signed-off-by: sairam1411 <majgaonkars3@gmail.com>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -1,10 +1,12 @@
 # Inherit common mobile Lineage stuff
 $(call inherit-product, vendor/lineage/config/common.mk)
 
+ifneq ($(WITH_GMS),true)
 # Default notification/alarm sounds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.notification_sound=Argon.ogg \
     ro.config.alarm_alert=Hassium.ogg
+endif
 
 # Apps
 PRODUCT_PACKAGES += \
